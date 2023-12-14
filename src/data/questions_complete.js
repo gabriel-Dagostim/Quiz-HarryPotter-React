a5805489c387cd67b588d093bfa7fa4c2c08590b
Gabriel Henrique Dagostim Simões <129344321+gabriel-Dagostim@users.noreply.github.com>
2023-12-14 14:39:42 -0300

    Update

---
+++ src/data/questions_complete.js
@@ -0,0 +1,285 @@
+const data = [
+  {
+    category: "Fácil",
+    questions: [
+      {
+        "question": "Qual é o nome completo do protagonista da série 'Harry Potter'?",
+        "options": ["Harry James Potter", "Harry James Poter", "Henrry James Potter", "Harry Potter"],
+        "answer": "Harry James Potter",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a casa em Hogwarts à qual Hermione Granger pertence?",
+        "options": ["Grifinória", "Sonserina", "Corvinal", "Lufa-Lufa"],
+        "answer": "Grifinória",
+        "tip": ""
+      },
+      {
+        "question": "Quem é o melhor amigo de Harry Potter?",
+        "options": ["Ron Weasley", "Sua coruja", "Hagrid", "Neville Longbottom"],
+        "answer": "Ron Weasley",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o animal de estimação de Rubeus Hagrid no segundo filme/livro?",
+        "options": ["Dragao", "Gato", "Cachorro", "Pelucio"],
+        "answer": "Cachorro",
+        "tip": "Hagrid adotou vários animais, mas seu mais notável é o dragão chama Norbert."
+      },
+      {
+        "question": "Qual é o nome do vilão principal na série 'Harry Potter'?",
+        "options": ["Severus Snape", "Sirius Black", "Alvo Dumbledore", "Lord Voldemort"],
+        "answer": "Lord Voldemort",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a escola de magia frequentada por Harry Potter?",
+        "options": ["Escola de Magia de Durmstrang", "Escola de Magia de Beauxbatons", "Escola de Magia de Ilvermorny", "Escola de Magia de Hogwarts"],
+        "answer": "Escola de Magia de Hogwarts",
+        "tip": ""
+      },
+      {
+        "question": "Como é chamado o esporte jogado em vassouras voadoras em Hogwarts?",
+        "options": ["Quebra-cranio", "Quadribol", "Batedores", "Vassoura-a-Vassoura"],
+        "answer": "Quadribol",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do irmão mais velho de Ron Weasley?",
+        "options": ["Fred Weasley", "Gui Weasley", "Percy Weasley", "George Weasley"],
+        "answer": "Gui Weasley",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a data do aniversário de Harry Potter?",
+        "options": ["31 de julho", "1 de setembro", "4 de junho", "7 de abril"],
+        "answer": "31 de julho",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o feitiço utilizado para conjurar uma luz na ponta da varinha?",
+        "options": ["Accio", "Expecto Patronum", "Lumos", "Expelliarmus"],
+        "answer": "Lumos",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do objeto que permite que um bruxo ou bruxa se esconda dos outros?",
+        "options": ["Varinha", "Penseira", "Relógio de Areia", "Capa de Invisibilidade"],
+        "answer": "Capa de Invisibilidade",
+        "tip": ""
+      },
+      {
+        "question": "Quem é o guardião das Chaves e Terrenos de Hogwarts?",
+        "options": ["Rubeus Hagrid", "Argus Filch", "Severus Snape", "Horácio Slughorn"],
+        "answer": "Rubeus Hagrid",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do pai de Harry Potter?",
+        "options": ["James Potter", "Sirius Black", "Alvo Dumbledore", "Remo Lupin"],
+        "answer": "James Potter",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a matéria que o Professor Snape mais quer ensinar em Hogwarts?",
+        "options": ["Poções", "Defesa Contra as Artes das Trevas", "Transformações", "Adivinhação"],
+        "answer": "Defesa Contra as Artes das Trevas",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome da coruja de Harry Potter?",
+        "options": ["Hedwig", "Errol", "Pigwidgeon", "Fawkes"],
+        "answer": "Hedwig",
+        "tip": ""
+      }
+    ],
+  },
+
+  {
+    category: "Médio",
+    questions: [
+      {
+        "question": "Qual é a casa em Hogwarts à qual Cedrico Diggory pertence?",
+        "options": ["Grifinória", "Sonserina", "Corvinal", "Lufa-Lufa"],
+        "answer": "Lufa-Lufa",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a matéria que o Professor Snape leciona em Hogwarts no terceiro ano de Harry a escola ?",
+        "options": ["Defesa Contra as Artes das Trevas", "Poções", "Transformações", "Herbologia"],
+        "answer": "Poções",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do fantasma que assombra o banheiro feminino no primeiro andar de Hogwarts?",
+        "options": ["Barão Sangrento", "Dama Cinzenta", "Nick-Quase-Sem-Cabeça", "Myrtle a Murta"],
+        "answer": "Myrtle a Murta",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o patrono de Severus Snape?",
+        "options": ["Cervo", "Cobra", "Corsa", "Lebre"],
+        "answer": "Corsa",
+        "tip": ""
+      },
+      {
+        "question": "Quantas casas existem em Hogwarts e quais são seus nomes?",
+        "options": ["4 casas: Greifinória, Sonserina, Corvonal e Lufa-Lufa", "4 casas: Grifinória, Sonserina, Corvinal e Lufa-Lufa", "4 casas: Grifinória, Sonserina, Ravenclau e Lufa-Lufa", "4 casas: Grefindaria, Sonserina, Corvinal e hufle-puft"],
+        "answer": "4 casas: Grifinória, Sonserina, Corvinal e Lufa-Lufa",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do jornal que Rita Skeeter escreve em 'O Cálice de Fogo'?",
+        "options": ["O Profeta Diário", "O Pasquim", "A Voz do Povo", "O Chicote Diário"],
+        "answer": "O Chicote Diário",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome da professora de Adivinhação em Hogwarts?",
+        "options": ["Professora Sprout", "Professora McGonagall", "Professora Trelawney", "Professora Flitwick"],
+        "answer": "Professora Trelawney",
+        "tip": ""
+      },
+      {
+        "question": "Quais são os nomes dos irmãos que são os protagonistas da história 'Os Três Irmãos' em 'Os Contos de Beedle, o Bardo'?",
+        "options": ["Harry, Ron e Hermione", "Alvo, Aberforth e Percival", "Antíoco, Cadmo e Ignoto", "Icaro, Dédalo e Teseu"],
+        "answer": "Antíoco, Cadmo e Ignoto",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do diretor da Escola de Magia de Durmstrang durante o Torneio Tribruxo?",
+        "options": ["Igor Karkaroff", "Viktor Krum", "Vladimir Grigorovich", "Ivan Volkov"],
+        "answer": "Igor Karkaroff",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a criatura mágica que é a guardiã da Fonte da Sorte em Hogwarts?",
+        "options": ["Hidra", "Testrálio", "Niffler", "Hipogrifo"],
+        "answer": "Testrálio",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do irmão mais velho de Ron Weasley?",
+        "options": ["Fred Weasley", "Gui Weasley", "Percy Weasley", "George Weasley"],
+        "answer": "Gui Weasley",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a criatura mágica que tem a habilidade de se transformar em qualquer coisa?",
+        "options": ["Anamago", "Veela", "Metamorfomago", "Licantropo"],
+        "answer": "Metamorfomago",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome da planta que toma conta da Casa dos Black em 'A Ordem da Fênix'?",
+        "options": ["Mandrágora", "Espirradeira", "Víbora-de-chifre", "Florflit"],
+        "answer": "Espirradeira",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do objeto de quadribol que deve ser capturado pelo apanhador?",
+        "options": ["Pomo de Ouro", "Balaço", "Pomorim dourado", "Goles"],
+        "answer": "Pomo de Ouro",
+        "tip": ""
+      },
+      {
+        "question": "O que acontece quando uma pessoa bebe a Poção Polissuco?",
+        "options": ["Ela fica invisível", "Ela adquire as habilidades do animal que deu o pelo", "Ela se transforma em uma pessoa completamente diferente por um tempo limitado", "Ela ganha a capacidade de voar"],
+        "answer": "Ela se transforma em uma pessoa completamente diferente por um tempo limitado",
+        "tip": ""
+      }
+    ],
+  },
+
+  {
+    category: "Difícil",
+    questions: [
+      {
+        "question": "Qual é o nome completo do ultimo dono da Varinha das Varinhas antes de Harry Potter?",
+        "options": ["Gellert Grindelwald", "Alvo Dumbledore", "Severus Snape", "Draco Malfoy"],
+        "answer": "Draco Malfoy",
+        "tip": ""
+      },
+      {
+        "question": "O que é o 'Princípio da Priori Incantatem' em relação às varinhas mágicas?",
+        "options": ["Um feitiço para descobrir a história de uma varinha.", "Um efeito que ocorre quando duas varinhas irmãs se encontram.", "Um termo usado para descrever a ligação entre um bruxo e sua varinha.", "Uma teoria sobre a origem das varinhas mágicas."],
+        "answer": "Um efeito que ocorre quando duas varinhas irmãs se encontram.",
+        "tip": ""
+      },
+      {
+        "question": "Quem foi o primeiro Ministro da Magia a aparecer nos livros de Harry Potter?",
+        "options": ["Cornélio Fudge", "Rufus Scrimgeour", "Millicent Bagnold", "Kingsley Shacklebolt"],
+        "answer": "Millicent Bagnold",
+        "tip": ""
+      },
+      {
+        "question": "O que a Professora Sybill Trelawney previu quando Harry Potter estava em seu terceiro ano em Hogwarts?",
+        "options": ["A morte de Dobby", "A volta de Voldemort", "A prisão de Hagrid", "A queda de Dumbledore"],
+        "answer": "A volta de Voldemort",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a criatura que guarda a entrada da Câmara Secreta em Hogwarts?",
+        "options": ["Acromântula", "Bicuço", "Basilisco", "Thestral"],
+        "answer": "Basilisco",
+        "tip": ""
+      },
+      {
+        "question": "O que é o 'Bicho-Papão' de Remo Lupin?",
+        "options": ["Lua cheia", "Seus pais mortos", "Morrer", "Dora Tonks"],
+        "answer": "Lua cheia",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome completo do fundador da Escola de Magia e Bruxaria de Ilvermorny nos Estados Unidos?",
+        "options": ["Isolt Sayre", "Seraphina Picquery", "Gormlaith Gaunt", "Artemis Fowl"],
+        "answer": "Isolt Sayre",
+        "tip": ""
+      },
+      {
+        "question": "Quem era o dono anterior da casa onde os Dursley moravam em Privet Drive, antes deles?",
+        "options": ["Família Granger", "Família Lovegood", "Família Evans", "Família Potter"],
+        "answer": "Família Evans",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a raça do dragão que guarda a caverna da Horcrux?",
+        "options": ["Rabo corneo hungaro", "Horntail Húngaro", "Nenhum dragao", "Vipertooth Peruano"],
+        "answer": "Nenhum dragao",
+        "tip": ""
+      },
+      {
+        "question": "Qual é a única maneira conhecida de destruir uma Horcrux?",
+        "options": ["Fogo maldito e a espada do godric grifinória", "veneno de basilisco", "Fogo maldito e bombarda maxima", "Fogo maldito e veneno de basilisco"],
+        "answer": "Fogo maldito e veneno de basilisco",
+        "tip": ""
+      },
+      {
+        "question": "Quem é o autor fictício de 'As Aventuras de Martin Miggs, o Traidor do Queima-Babas'?",
+        "options": ["Ludwig Beethoven", "Beedle, o Bardo", "Newt Scamander", "Miranda Goshawk"],
+        "answer": "Miranda Goshawk",
+        "tip": ""
+      },
+      {
+        "question": "O que acontece quando um bruxo ou bruxa bebe a Poção de Felix Felicis em excesso?",
+        "options": ["Ganha habilidades de metamorfomago", "Faz com que todos em volta fiquem com sorte limitada", "Tem sorte sobrenatural por um tempo limitado", "Entra em um estado de euforia constante"],
+        "answer": "Entra em um estado de euforia constante",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome do instrumento mágico usado para detectar os batimentos cardíacos e emoções de uma pessoa?",
+        "options": ["Detetor de Emoções", "Detector de Batimentos Mágicos", "Varinha Sismográfica", "Sondeador de Sentimentos"],
+        "answer": "Detector de Batimentos Mágicos",
+        "tip": ""
+      },
+      {
+        "question": "Qual é o nome da poção que cria uma ligação inquebrável entre o bebedor e quem a deu, como a Poção de Unicórnio?",
+        "options": ["Poção da Amizade Eterna", "Poção de Confiança Total", "Poção do Amor Perdido", "Poção do Lírio Rasteiro"],
+        "answer": "Poção do Amor Perdido",
+        "tip": ""
+      }
+    ],
+  },
+];
+
+export default data;
